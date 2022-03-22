363d6a0f20e34c3970305385eb899dc971200af0
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-03-22 21:03:36 +0300

    feat: add main page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="form-wrapper">
+        <h1>Анализатор страниц</h1>
+        <h2>Бесплатно проверяйте сатый на SEO пригодность</h2>
+        <form action="{{ route('urls.store') }}" method="post">
+            @csrf
+            <input type="text" name="url[name]" placeholder="https://www.expample.com">
+            <input type="submit" value="Проверить">
+        </form>
+    </div>
+@endsection
