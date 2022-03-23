a006c81a7caf8232e5bab403c0caf44a5fbb1037
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-03-23 21:32:58 +0300

    add url_checks table migration

---
+++ database/migrations/2022_03_23_171023_create_url_checks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('url_checks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('url_id');
+            $table->unsignedSmallInteger('status_code')->nullable();
+            $table->string('h1')->nullable();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('url_checks');
+    }
+};
