ecf48e4f475eaad47026569ceadc63c297158ac8
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-03-23 21:36:09 +0300

    add url check form and table with checkings

--- resources/views/url/show.blade.php
+++ resources/views/url/show.blade.php
@@ -16,4 +16,31 @@
             <td>{{ $url->created_at }}</td>
         </tr>
     </table>
+    <div>
+        <h2>Проверки</h2>
+        <form action="{{ route('urls.check', $url->id) }}" method="post">
+            @csrf
+            <input type="submit" value="Запустить проверку">
+        </form>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Код ответа</th>
+                <th>h1</th>
+                <th>title</th>
+                <th>description</th>
+                <th>Дата создания</th>
+            </tr>
+            @foreach ($checks as $check)
+                <tr>
+                    <td>{{ $check->id }}</td>
+                    <td>{{ $check->status_code }}</td>
+                    <td>{{ $check->h1 }}</td>
+                    <td>{{ $check->title }}</td>
+                    <td>{{ $check->description }}</td>
+                    <td>{{ $check->created_at }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
