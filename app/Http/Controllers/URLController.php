d78f01be907d6b6b39842655821864eac0c18196
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-03-24 21:47:08 +0300

    fix: add check for empty form

--- app/Http/Controllers/URLController.php
+++ app/Http/Controllers/URLController.php
@@ -36,7 +36,6 @@ class URLController extends Controller
 
     public function show($id)
     {
-
         $url = DB::table('urls')->find($id);
         $checks = DB::table('url_checks')
             ->where('url_id', $id)
@@ -48,7 +47,7 @@ class URLController extends Controller
     public function store(Request $request)
     {
         $data = $request->input('url.name');
-        $url = $this->normalizeURL($data);
+        $url = is_null($data) ? false : $this->normalizeURL($data);
         if (!$url) {
             return back()
                 ->with('error', 'Некорректный URL')
@@ -96,7 +95,12 @@ class URLController extends Controller
     private function normalizeURL(string $url): string|false
     {
         $urlParts = parse_url($url);
-        if ($urlParts === false || !isset($urlParts['scheme']) || !isset($urlParts['host']) || strlen($url) > 255) {
+        if (
+            $urlParts === false
+            || !isset($urlParts['scheme'])
+            || !isset($urlParts['host'])
+            || strlen($url) > 255
+        ) {
             return false;
         }
         return $urlParts['scheme'] . '://' . $urlParts['host'];
